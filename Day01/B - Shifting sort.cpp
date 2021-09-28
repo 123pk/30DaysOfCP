aab70ef87fe5b3f3310dd5d32c89c9399d1ef11e
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-28 23:25:44 +0530

    Create B - Shifting sort.cpp

---
+++ Day01/B - Shifting sort.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforce Round #744 Div 3
+Approach :- we will make changes until the array is sorted , we will make a copy of sorted array and sort that copy array , now we will check for the mismatched element in both array
+            we will then look for the actual value which should be present at that position in sorted array by looking at copy array and we will make left shift from that 
+            ( actual_value index to mismatched_value index ).
+*/
