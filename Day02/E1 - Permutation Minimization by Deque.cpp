fc3fdb0754611c9113de5cf1f12bf6b8e163395c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-29 20:33:26 +0530

    Update E1 - Permutation Minimization by Deque.cpp

--- Day02/E1 - Permutation Minimization by Deque.cpp
+++ Day02/E1 - Permutation Minimization by Deque.cpp
@@ -5,3 +5,40 @@ Approach :- Use a array of size 2*n and store the first element at index 'n' and
             element , now when we get any new element we compare with the element at 'l' if ( A['l'] > curr_Val) then we add it the front and decrement 'l' else we add to the end
             and increment 'r' . Now print the array.
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+         int n;
+         cin>>n;
+         
+         int A[n];
+         for(int i=0;i<n;++i){
+             cin>>A[i];
+         }
+         
+         vector<int>ans(2*n);
+         int l=n,r=n;
+        
+         for(int i=0;i<n;++i){
+             if(i==0)ans[l]=A[i];
+             else{
+                 if(ans[l]>A[i]){
+                     l--;
+                     ans[l]=A[i];
+                 }
+                 else{
+                     r++;
+                     ans[r]=A[i];
+                 }
+             }
+         }
+         
+         for(int i=l;i<=r;++i)cout<<ans[i]<<' ';
+         cout<<"\n";
+    }
+}
