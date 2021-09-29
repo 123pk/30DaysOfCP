e7e780c9bdc1dbcc2e9671397d7a76b5b1365bc8
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-29 20:07:55 +0530

    Create C - Ticks.cpp

---
+++ Day02/C - Ticks.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces 
+Contest :- Codeforces Round 744 Div 3
+Approach :- We consider each point having '*' to be mid point and from there we try to go to left diagonally upward and similarly right diagonally upward , if we are able to get a 
+            valid tick of size > k then we mark all the cells of grid , at the end we check if all the point of grid having '*' are marked or not , if marked we print "yes" else "NO"
+*/
