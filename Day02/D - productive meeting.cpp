e09f4decd4bcd9401ca6fd6f1d8ad523650b3710
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-29 20:24:29 +0530

    Create D - productive meeting.cpp

---
+++ Day02/D - productive meeting.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 744 Div 3
+Approach :- We use a max heap to store all the values and we take the two max values of the heap and make pair of them and if any they becomes '0' then we leave it else we push 
+            value-1 in the heap . We create max heap of pair of of values and index .
+*/
