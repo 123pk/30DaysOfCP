c138da9afb28a12e091100df8b3e0221c030a8f1
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-02 14:26:18 +0530

    Update C - Ternary Palindrome.cpp

--- Day05/C - Ternary Palindrome.cpp
+++ Day05/C - Ternary Palindrome.cpp
@@ -7,3 +7,31 @@ Approach :- This problems also can be solved by just analysing the problem , it 
            as there are 3 unique characters atmax our string can have and all different permutaion of 3 character is 3! = 6.
            if difference between max count of character and min count of character is > 1 then we cannot have any valid permutation in that case ans is '0'.
 */
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    while(t--){
+           string s;
+           cin>>s;
+           int n=s.size();
+           map<char,int>P;
+           for(int i=0;i<n;++i)P[s[i]]++;
+
+           int A[3]={P['0'],P['1'],P['2']};
+           sort(A,A+3);
+           if(A[2]==A[0])cout<<6<<"\n";
+           else if(A[2]-A[0]>1)cout<<0<<"\n";
+           else if(n==1)cout<<"2\n";
+           else if(A[2]==A[1] || A[1]==A[0]) cout<<2<<"\n";
+           else cout<<"1\n";
+	
+    }
+}
