ee98c3b46ae1edbeafcc07c2c3b0e56964f99f91
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-02 14:24:34 +0530

    Create C - Ternary Palindrome.cpp

---
+++ Day05/C - Ternary Palindrome.cpp
@@ -0,0 +1,8 @@
+/*
+Platform :- Hackerearth
+Contest :- Hackerearth October easy 2021
+Approach :- This problems also can be solved by just analysing the problem , it states that we want permutation of string with atmost 'n' palindromic substring , 
+           you should know that stirng of 1 character is a palindrome so a given string already have atleast 'n' palindromes , so you want to arrange elements such that we 
+           don't have palindrome of size>1 ,which can be done by arraning stirng in some pattern like this "123123.." or "213213.. etc , so max answer is '6' when we have count of all unque character equal
+           as there are 3 unique characters atmax our string can have and all different permutaion of 3 character is 3! = 6.
+*/
