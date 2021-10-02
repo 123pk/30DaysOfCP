4888d6dc5c6157dfedb2a52376e2305d93c49e07
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-02 14:14:55 +0530

    Update B - Good string .cpp

--- Day05/B - Good string .cpp
+++ Day05/B - Good string .cpp
@@ -6,3 +6,35 @@ Approach :- If you carefully read the problem and analyse it you will find that 
             ans is total_pair_possible - (all_ones * all_zeros).
             Total number of pair for 'n' size array is [ (n*(n-1))/2 ]
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int t;
+	cin>>t;
+
+	while(t--){
+		int n;
+		cin>>n;
+        
+		int c=0,d=0;
+		vector<string>P(n);
+		for(int i=0;i<n;++i){
+			cin>>P[i];
+			sort(P[i].begin(),P[i].end());
+			if(P[i][0]==P[i][P[i].size()-1]){
+				if(P[i][0]=='0')c++;
+				else d++;
+			}
+		}
+         
+		long long ans=n;
+		ans*=(n-1);
+		ans/=2;
+
+		long long temp=c;
+		temp*=d;
+		ans-=temp;
+		cout<<ans<<"\n";
+	}
+}
