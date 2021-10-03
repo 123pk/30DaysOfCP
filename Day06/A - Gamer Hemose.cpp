3fb3936fc2d435927ac5ce9f12545a88493888f3
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-03 23:08:02 +0530

    Create A - Gamer Hemose.cpp

---
+++ Day06/A - Gamer Hemose.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Contest 746 Div 2
+Approach :- so we need only two greatest values of array and we can simulatneously take one of them and use them .
+*/
