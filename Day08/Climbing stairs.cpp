09fc8d678d11d125938728e55d9e475619500b72
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-05 22:10:47 +0530

    Update Climbing stairs.cpp

--- Day08/Climbing stairs.cpp
+++ Day08/Climbing stairs.cpp
@@ -1,6 +1,6 @@
 /*
 Platform :- Leetcode
-Approach :- It is a standard dp problem where no_of_way to reach 'ith' step is ( no_of_ways_to reach 'i-1'+'i-2')  or (dp[i] = dp[i-1]+dp[i-2] ) where dp[0]=dp[1]=1;
+Approach :- It is a standard dp problem where no_of_way to reach 'ith' step is ( no_of_ways_to reach 'i-1'+'i-2')  or (dp[i] = dp[i-1]+dp[i-2] ) where dp[0] = dp[1] = 1;
 */
 class Solution {
 public:
