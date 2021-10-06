3e5387530f6d9bacea329155d0ba418c381caa47
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-06 20:04:12 +0530

    Create Find all duplicates in Array.cpp

---
+++ Day09/Find all duplicates in Array.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Leetcode
+Subtask :- Solve in O(n) time 
+Approach :- As the value in array is in range ( 1 to 10^5 ) then we can use a frequency of array of size n+1 and store the frequency of each element in array and its runtime is 
+            O(n) in worst case now we will traverse through frequency array and if the frequency of element is greater than '1' then we add it to answer array .
+*/
