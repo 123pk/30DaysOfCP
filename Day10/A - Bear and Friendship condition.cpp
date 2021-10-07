6497bf5399e9341d172c5ee9a3a0458c0fc2a23f
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-07 23:28:32 +0530

    Create A - Bear and Friendship condition.cpp

---
+++ Day10/A - Bear and Friendship condition.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Codeforces
+Contest :- VK-Cup Round 1 2017
+Approach :- For a node to satisfy the condtion it need to be part of sub graph of nodes 'n' have no of indices equal to (n*(n+1))/2,  so we will store the count of each node edge
+            now we will run a bfs on unvisited nodes and calculate the number of nodes in that disjont segment/subgraph and also find number of vertices in it , if it doesnot 
+            satisfy the codition we print "NO" else at the end "YES"
+*/
