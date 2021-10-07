cac3ddf64c26c2b06e4067a266fbc3b59b1d9545
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-07 23:23:42 +0530

    Update A - Sequence Comparisons.kt

--- Day10/A - Sequence Comparisons.kt
+++ Day10/A - Sequence Comparisons.kt
@@ -4,3 +4,33 @@ Contest :- Kotlin Heros Round 8
 Approach :- if there is both '>' and '<' in string then we have multiple answers and our answer is '?' , no we are left with case when we have either one of type and we print that
             type.
 */
+fun main(args: Array<String>) {
+    val t = Integer.valueOf(readLine())
+    for(i in 1..t)
+    { 
+      
+      val s=readLine()!!
+      //var start = 1000
+      var c=0
+      var d=0
+      
+      for(j in s.indices){
+         if(s[j]=='<')c+=1
+         else{
+           if(s[j]=='>'){
+             d+=1
+           }
+         }
+      }
+       
+      if(c!=0 && d!=0)println('?')
+      else{
+         if(c!=0)println('<')
+         else {
+          if(d!=0)println('>')
+          else println('=')
+          }
+          
+      }
+    }
+}
