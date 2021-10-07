d40000529eea7785956da2dc611ec6a0fde5c4cb
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-07 23:23:18 +0530

    Create A - Sequence Comparisons.kt

---
+++ Day10/A - Sequence Comparisons.kt
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Kotlin Heros Round 8
+Approach :- if there is both '>' and '<' in string then we have multiple answers and our answer is '?' , no we are left with case when we have either one of type and we print that
+            type.
+*/
