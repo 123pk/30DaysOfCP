434959a21319439f438e59d4019aba5fc9b75fe2
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-08 23:14:40 +0530

    Create C - Make them equal.cpp

---
+++ Day11/C - Make them equal.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 747 Div 2
+Approach :- If you will carefully ananlyse the problem , you will find that max no of operation is going to be 2 and min is going to be 0.
+            so when all characters of string are equal to char ch then ans =0
+            else we take account of index of characters didn't match and greedily choose the values of 'x'
+*/
