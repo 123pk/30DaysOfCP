9002c1a265d83fdf16a83c8f5f003ff5dc36b6df
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-08 23:09:18 +0530

    Update A - Consecutive Sum riddle.cpp

--- Day11/A - Consecutive Sum riddle.cpp
+++ Day11/A - Consecutive Sum riddle.cpp
@@ -3,3 +3,18 @@ Platform :-Codeforces
 Contest :- Codeforces Round 747
 Approach :- for acheiving any sum 'n' we can use (-(n-1) to n) range as all the -ve values will cancel out all the positive values till (n-1) and at the end we will have 'n' only
 */
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+         long long n;
+         cin>>n;
+         
+         cout<<(-(n-1))<<" "<<n<<"\n";
+        
+    }
+}
