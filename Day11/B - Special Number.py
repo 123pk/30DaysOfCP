0b187d80c9bd933d3a4a9848abc1a38d6a8cc52b
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-08 23:08:32 +0530

    Create B - Special Number.py

---
+++ Day11/B - Special Number.py
@@ -0,0 +1,5 @@
+'''
+Platform :- Codeforces
+Contest :- Codeforces Round 747 Div 2
+Approach :- First convert 'k' into binary stiring and itterate through it if the s[i]=='1' the add pow(a,i) to your answer take care of modulo
+'''
