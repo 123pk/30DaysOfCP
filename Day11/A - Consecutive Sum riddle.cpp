9b9c5562ab5969708e356b1f4343a71c2d322c6e
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-08 22:53:15 +0530

    Create A - Consecutive Sum riddle.cpp

---
+++ Day11/A - Consecutive Sum riddle.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :-Codeforces
+Contest :- Codeforces Round 747
+Approach :- for acheiving any sum 'n' we can use (-(n-1) to n) range as all the -ve values will cancel out all the positive values till (n-1) and at the end we will have 'n' only
+*/
