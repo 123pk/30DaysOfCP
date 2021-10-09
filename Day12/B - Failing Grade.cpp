1ee96b4b8fdd79ef6c7af2bea40d564b815f018b
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-09 23:02:38 +0530

    Update B - Failing Grade.cpp

--- Day12/B - Failing Grade.cpp
+++ Day12/B - Failing Grade.cpp
@@ -3,3 +3,18 @@ Platform :- Atcoder
 Contest :- Atcoder Beginner Contest 222
 Approach :- while taking input check and count values if they are less than 'p'
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,p;
+    cin>>n>>p;
+    
+    int A[n];
+    int c=0;
+    for(int i=0;i<n;++i){
+        cin>>A[i];
+        if(A[i]<p)c++;
+    }
+    cout<<c<<"\n";
+}
