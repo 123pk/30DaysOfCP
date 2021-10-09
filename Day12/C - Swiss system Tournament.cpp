7661352662fbba15165d2ea4502f8015aae69d81
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-09 23:05:47 +0530

    Create C - Swiss system Tournament.cpp

---
+++ Day12/C - Swiss system Tournament.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Atcoder
+Contest :- Atcoder Beginner Contest 222
+Approach :- we use a vector of pair to store the points and index of each values now we will play m games and in each game we will conduct matches between 1-->2 , 3-->4 and so on
+            and after each game we update the values in increasing order .
+*/
