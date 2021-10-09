a60e9c6796140b1144e3bac4edef9328cf00a7b6
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-09 23:00:37 +0530

    Update A - Four Digits.cpp

--- Day12/A - Four Digits.cpp
+++ Day12/A - Four Digits.cpp
@@ -2,3 +2,16 @@
 Platform :- Atcoder
 Contest :- Atcoder Beginner Contest 222
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+    
+    if(s.size()==4)cout<<s<<"\n";
+    else {
+        for(int i=1;i<=(4-s.size());++i)cout<<"0";
+        cout<<s<<"\n";
+    }
+}
