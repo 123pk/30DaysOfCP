1d3c0f0a5eef938af0729992d944d4ae9bd3e0ac
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-10 11:20:14 +0530

    Create Two out of Three.cpp

---
+++ Day13/Two out of Three.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Contest :- Leetcode Weekly Contest 262
+Approach :- store each element of all the three array in set and also store frequency using map and now itterate through set and if the frequency of element is >=2 add to answer.
+*/
