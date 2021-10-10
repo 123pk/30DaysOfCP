a86ecd358e0f9d109fa8385bc319f8bd4ca449e3
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-10 11:14:28 +0530

    Create Minimum operations to make a uni-value grid.cpp

---
+++ Day13/Minimum operations to make a uni-value grid.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Leetcode
+Contest :- Leetcode Weekly contest 262
+Approach :- First check if given array and 'x' are valid and has solution or not . We will have solution if all the element give same value when we do modulo with 'x' else return -1.
+            now we will take all the elements of grid and store in vector and sort it and choose the middle element of vector and convert all elements to that elements and find the cost
+            it is going to be our answer.
+*/
