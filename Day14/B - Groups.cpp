4589af141e55d6b32385fc294c4399c279c45704
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-11 22:23:47 +0530

    Create B - Groups.cpp

---
+++ Day14/B - Groups.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Codeforces 
+Contest :- Codeforces Educational Round 115
+Approach :- Find the number of students that are interested in taking classes on 'ith' day out of 5 days, now take all those days in account where no of studetns is >= half ( n/2 )
+            now look for all the pairs of days and check if total( summation of ) no of unique students interested in learning that day is equal to 'n' or not , if 'n' then yes we 
+            can get two groups of equal size and on different days .
+*/
