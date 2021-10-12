5ae6205917c4585c86dbdb842740796c5bce9511
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-12 23:52:07 +0530

    Update Nice pair.cpp

--- Day15/Nice pair.cpp
+++ Day15/Nice pair.cpp
@@ -1,4 +1,5 @@
 /*
 Platform :- Codechef 
 Contest :- Codechef Starter 15
+Approach :- There are on 9 digits so for each index check for next 9 index if you find any index that satisfy our condition then increment answer
 */
