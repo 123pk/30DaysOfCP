c4e1bd7d2b8791cb5ad4593fbdc1927e120070a3
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-13 22:26:13 +0530

    Create A - Elections.cpp

---
+++ Day16/A - Elections.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 748 Div 3
+Approach :- find the max value out of 3 and let (val = max_of_all + 1) , if there are more than one max in than case for them we print 1 else 0, and for other non max values answer
+            is (val-value)
+*/
