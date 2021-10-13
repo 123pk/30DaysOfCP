530afcf702a16cc3948cf4d421a342f4bb4d6707
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-13 22:32:51 +0530

    Create C - Save more mice.cpp

---
+++ Day16/C - Save more mice.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 748 Div 3
+Approach :- sort the values in decreasing order and start with first value after sorting if the time taken to get to his turn is >= A[i] then cat can eat him else he will reach
+           the hole and time take +=(n-A[i]).
+*/
