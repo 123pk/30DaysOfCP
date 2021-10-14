770d536ab1f1e1322061cad36605a919185b72a2
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-14 22:58:43 +0530

    Create E - Gardner and Trees.cpp

---
+++ Day17/E - Gardner and Trees.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 748 Div 3
+Approach :- we take path from to the root and if take account of 'k', after k operations if there is some edge between node we leave the nodes else we increment the count when we have
+            nodes with zero edges to them
+*/
