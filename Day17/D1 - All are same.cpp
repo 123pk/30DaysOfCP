6dd30476d2993fc18c92c5dca6b6a151e35a5d2b
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-14 22:55:34 +0530

    Create D1 - All are same.cpp

---
+++ Day17/D1 - All are same.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 748 Div 3
+Approach :- You need to analyse it then you will find there is only one case when we have no solution and it is when all the elemnts are equal else the ans is the gcd of all values
+            A[i]-A[0] for i>0 ,
+*/
