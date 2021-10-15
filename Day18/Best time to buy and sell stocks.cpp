731c2fffa23e8c6189424e576b199d611b17f18a
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-16 00:17:33 +0530

    Update Best time to buy and sell stocks.cpp

--- Day18/Best time to buy and sell stocks.cpp
+++ Day18/Best time to buy and sell stocks.cpp
@@ -3,25 +3,34 @@ Platform :- Leetcode
 */
 class Solution {
 public:
-    int maxProfit(vector<int>& p) 
-{
-	if (p.size() < 2)
-		return 0;
-	int i, sz = p.size();
-	int ret = 0;
-	vector<int> buy(sz, 0);
-	vector<int> sell(sz, 0);
-	buy[0] = -p[0];
-	for (i = 1; i < sz; ++i)
-	{
-		sell[i] = max(buy[i - 1] + p[i], sell[i - 1] - p[i - 1] + p[i]);
-		if (ret < sell[i]) //record the max sell[i]
-			ret = sell[i];
-		if (1 == i)
-			buy[i] = buy[0] + p[0] - p[1];
-		else
-			buy[i] = max(sell[i - 2] - p[i], buy[i - 1] + p[i - 1] - p[i]);
-	}
-	return ret;
-}
+    int maxProfit(vector<int>& price){
+        //we do things in reverse 
+        //max value for i -- n is some [x] + dp[i+z]
+        //dp[n-1]=0
+        //dp[n-2]=max(0,prices[i+1]-price[i])
+        int n = price.size();
+        vector<int>dp(n);
+        
+        dp[n-1]=0;
+        if(n==1)return dp[0];
+        
+        dp[n-2]=max(0,price[n-1]-price[n-2]);
+        
+        if(n==2)return dp[0];
+        
+        for(int i=n-3;i>=0;--i){
+            //find some x for some index j and add with dp[j+2]
+            dp[i]=dp[i+1];
+            for(int j=i+1;j<n;++j){
+                if((j+2)<n){
+                    dp[i]=max(dp[i],max(0,price[j]-price[i])+dp[j+2]);
+                }
+                else{
+                    dp[i]=max(dp[i],max(0,price[j]-price[i]));
+                }
+            }
+        }
+        
+        return dp[0];
+    }
 };
