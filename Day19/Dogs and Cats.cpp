8fcdf349881b7da77a8017125360a73990641a05
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-16 21:49:03 +0530

    Create Dogs and Cats.cpp

---
+++ Day19/Dogs and Cats.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Coding competitions with google
+Approach :- maintain a vis array to store the indexes that you have visited and traverse through the array and check if you can feed the current animal or not and mark it if 
+            you feeded it else break and run a loop from 0 to n-1 and check if all the indexes with dogs are feeded or not . If not print "YES" else "NO"
+*/
