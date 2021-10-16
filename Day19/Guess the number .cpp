15ef74822da6fe949f6968b3e831ac181c63d31e
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-16 22:46:03 +0530

    Create Guess the number .cpp

---
+++ Day19/Guess the number .cpp
@@ -0,0 +1,4 @@
+/*
+Platform :- Leetcode
+Approach :- Do basic binary search and check for all mid values , start = 1, end = n , and for finding mid and avoiding interger overflow use ( mid = (end-start)/2 + start )
+*/
