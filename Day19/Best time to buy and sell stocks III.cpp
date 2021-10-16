a76b9e7d224c27b7619c77bc55f88cec40bcbb89
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-16 22:28:19 +0530

    Update Best time to buy and sell stocks III.cpp

--- Day19/Best time to buy and sell stocks III.cpp
+++ Day19/Best time to buy and sell stocks III.cpp
@@ -3,5 +3,5 @@ Platform :- Leetcode
 Approach :- Since we can do atmost 2 transaction we can explot this , first find the max profit we can make in case of 1 transaction and now for store prefix array and suffix array
             prix[i] gives us the smallest values till ith index and suff[i] gives us the biggest values till ith index . Now make one more array which store the max profit till ith
             index and that can be max of all values of ( prices[j] - pref[j] )  for ( 0 <= j <= i) , and now do a final traversal where you will make partition array into two 
-            ( 0 to i ) and (i to n-1 ) for all ( 0 <= i <n-1 ) and find the max of ans and sum of max of two sub arrays .
+            ( 0 to i ) and (i+1 to n-1 ) for all ( 0 <= i <n-1 ) and find the max of ans and sum of max of two sub arrays .
 */
