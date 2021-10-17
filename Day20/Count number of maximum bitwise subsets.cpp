24f2b654182642d4cb2e30b0fc5efc524cc9275e
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-17 21:04:13 +0530

    Update and rename Count number of maximum bitwise subsets.cpp to Day20/Count number of maximum bitwise subsets.cpp

--- Day20/Count number of maximum bitwise subsets.cpp
+++ Day20/Count number of maximum bitwise subsets.cpp
@@ -1,5 +1,5 @@
 /*
 Platform :- Leetcode
-Contest :- Weekly contest 
+Contest :- Weekly contest 263
 Approach :- since the value of n is 16 , we will try every subset and check store the frequency of the "OR" values of that subset at the end we will return the count of max value
 */
