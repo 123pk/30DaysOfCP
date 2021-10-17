682f07dad31cb9e9fcfd61a0ee790dd4aaace253
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-17 21:10:06 +0530

    Create Simple Bank system.cpp

---
+++ Day20/Simple Bank system.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Leetcode
+Contest :- Leetcode Weekly contest 263
+Approach :- take care of valid accounts while doing any query and transaction , maintain a normal vector of long long and on withdraw change the amount of sender and receiver 
+            also take care of cases when the transfer is possible and when it is not.
+*/
