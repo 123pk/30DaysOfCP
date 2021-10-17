a1226612e2b5d01420c47f5bd945dae012845bb7
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-17 21:01:51 +0530

    Create Count number of maximum bitwise subsets.cpp

---
+++ Count number of maximum bitwise subsets.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Contest :- Weekly contest 
+Approach :- since the value of n is 16 , we will try every subset and check store the frequency of the "OR" values of that subset at the end we will return the count of max value
+*/
