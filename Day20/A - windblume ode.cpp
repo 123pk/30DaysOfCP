e8a9840dcaa34ef45bfcd1d878d17f8e2f1aa1b3
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-17 21:24:26 +0530

    Create A - windblume ode.cpp

---
+++ Day20/A - windblume ode.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round #749 (Div. 1 + Div. 2, based on Technocup 2022 Elimination Round 1)
+Approach :- find the sum of all elements , if it is even then whole array is good else if it is odd prime then we need to remove one of odd values and answer is n-1 removing that one
+            odd value , else in all other cases whole array is good
+*/
