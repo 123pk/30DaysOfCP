c9f209a8f99f224ea1e5962644697b879e7dfd06
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-17 21:05:54 +0530

    Update Count number of maximum bitwise subsets.cpp

--- Day20/Count number of maximum bitwise subsets.cpp
+++ Day20/Count number of maximum bitwise subsets.cpp
@@ -3,3 +3,22 @@ Platform :- Leetcode
 Contest :- Weekly contest 263
 Approach :- since the value of n is 16 , we will try every subset and check store the frequency of the "OR" values of that subset at the end we will return the count of max value
 */
+class Solution {
+public:
+   
+    int countMaxOrSubsets(vector<int>& nums) {
+        map<int,int>P;
+        int n=nums.size();
+        for(int i=1;i<=(1<<n);++i){
+            int val=0;
+            for(int j=0;j<n;++j){
+                if(i&(1<<j)){
+                    val|=nums[j];
+                }
+            }
+            P[val]++;
+        }
+        
+        return P.rbegin()->second;
+    }
+};
