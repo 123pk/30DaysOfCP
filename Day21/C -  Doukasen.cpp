c478922971ee3db795e62469256ce49c7cadc858
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-18 11:23:28 +0530

    Create C -  Doukasen.cpp

---
+++ Day21/C -  Doukasen.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Atcoder
+Contest :- Atcoder Beginner Contest 223
+Approach :- Find the total_time taken to travel whole distance which is sum(A[i]/B[i]) now the time of collision is (total_time/2) , now total distance travelled in
+            this time is our answer.
+*/
