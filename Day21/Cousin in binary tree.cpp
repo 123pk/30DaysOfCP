f3261c82b082da69664b0f7ac5d82126e632372a
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-18 21:30:10 +0530

    Create Cousin in binary tree.cpp

---
+++ Day21/Cousin in binary tree.cpp
@@ -0,0 +1,4 @@
+/*
+Platform :- Leetcode
+Approach :- Search for 'x' and 'y' in tree and take care of their parent and their height if height is same and parent are different then they are cousins else they are not 
+*/
