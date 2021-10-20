7768bafe8323c0f5b88bed99ca42439259e91221
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-20 23:16:10 +0530

    Update Reverse words in a string.cpp

--- Day23/Reverse words in a string.cpp
+++ Day23/Reverse words in a string.cpp
@@ -4,3 +4,33 @@ Approach :- first take out all the words from string , basic idea is add one ext
         if it is not empty then add that word in vector of string whenever you find ' ' , now reverse that vector and add all the words in string and add ' ' between words and don't
         add at the end of last word.
 */
+class Solution {
+public:
+    string reverseWords(string s) {
+        s+=' ';
+        vector<string>P;
+        
+        string ans;
+        string temp;
+        int f=0;
+        for(int i=0;i<s.size();++i){
+            if(s[i]==' '){
+                if(temp.size()){
+                    P.push_back(temp);
+                    temp="";
+                }
+            }
+            else {
+                temp+=s[i];
+            }
+        }
+        reverse(P.begin(),P.end());
+        for(auto &x:P){
+            ans+=x;
+            ans+=' ';
+        }
+        int n=ans.size();
+        ans.erase(ans.begin()+n-1);
+        return ans;
+    }
+};
