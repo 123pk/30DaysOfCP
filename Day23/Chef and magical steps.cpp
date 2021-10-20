2f16e024e3e3d4e6c1208d47d49704e0976f1fb9
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-20 23:24:49 +0530

    Update Chef and magical steps.cpp

--- Day23/Chef and magical steps.cpp
+++ Day23/Chef and magical steps.cpp
@@ -1,5 +1,5 @@
 /*
 Platform :- Codechef
-Contest :- Codechef starters 16( unrated Div 2)
+Contest :- Codechef starters 16 (unrated Div 2)
 Approach :- Firt precalculate all the primes till 10^7 and also precalculate the cost of reaching each step starting from step 1,now for given x and y , ans is (ans[y]-ans[x+1])+1
 */
