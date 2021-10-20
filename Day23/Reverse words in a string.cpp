169bf43777a2fc32017ae3cdedec2379f088f317
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-20 23:15:44 +0530

    Create Reverse words in a string.cpp

---
+++ Day23/Reverse words in a string.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Leetcode
+Approach :- first take out all the words from string , basic idea is add one extra space in string and now when ever you find a ' ' and maintain a tempory string to take care of words
+        if it is not empty then add that word in vector of string whenever you find ' ' , now reverse that vector and add all the words in string and add ' ' between words and don't
+        add at the end of last word.
+*/
