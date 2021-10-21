b047fefdfba079c6cf6859f2fc77b5addbe71002
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-21 20:37:09 +0530

    Update Insert delete Get random in O(1).cpp

--- Day24/Insert delete Get random in O(1).cpp
+++ Day24/Insert delete Get random in O(1).cpp
@@ -2,3 +2,34 @@
 Platform :- Leetcode
 Approach:- We will use a vector and unordered map to implement the three functions.
 */
+class RandomizedSet {
+public:
+
+    RandomizedSet() {
+        
+    }
+    
+    bool insert(int val) {
+        if (m.find(val) != m.end()) return false;
+        nums.emplace_back(val);
+        m[val] = nums.size() - 1;
+        return true;
+    }
+    
+    bool remove(int val) {
+        if (m.find(val) == m.end()) return false;
+        int last = nums.back();
+        m[last] = m[val];
+        nums[m[val]] = last;
+        nums.pop_back();
+        m.erase(val);
+        return true;
+    }
+    
+    int getRandom() {
+        return nums[rand() % nums.size()];
+    }
+private:
+    vector<int> nums;
+    unordered_map<int, int> m;
+};
