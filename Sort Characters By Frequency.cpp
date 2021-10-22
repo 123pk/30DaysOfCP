21a1492bc3775671f85c4fd72c7cfd5e5a8e7106
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-22 19:55:21 +0530

    Create Sort Characters By Frequency.cpp

---
+++ Sort Characters By Frequency.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Problem :- Sort Characters By Frequency
+Approach :- use a vector to store the frequency of each character and now create a pair and sort with characters frequency using comparator function. 
+*/
