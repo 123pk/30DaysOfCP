346d8c648accf2b32b940fa37cfe997eacc65908
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-23 21:03:01 +0530

    Create C - Game on leaves.cpp

---
+++ Day26/C - Game on leaves.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 646 Div 2
+Approach :- if either there is just one node or the target node 'x' is already a leaf then "Ayush" wins else we need to delete (n-1)edges and if it is odd then "Ayush" wins.
+*/
