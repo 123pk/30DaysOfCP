cf6a1f7c1cee4f73b5e9ab58c74c99dfe038441e
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-23 22:05:25 +0530

    Create Dance moves.cpp

---
+++ Day26/Dance moves.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codechef
+Contest :- Codechef Snackdown Round 1A
+Approach :- If x>y then we can only go backward and it will take (x-y)steps , else we can go forward by "abs(x-y)" if it is divisible by "2" else we need to move one "(x-y)/2+1"
+            and then come backward.
+*/
