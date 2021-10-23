b2d5eb273c8834ddc6f17b1d8991b457549d017c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-23 22:08:34 +0530

    Update Round Robin ranks.cpp

--- Day26/Round Robin ranks.cpp
+++ Day26/Round Robin ranks.cpp
@@ -2,3 +2,24 @@
 Platform :- Codechef
 Contest :- Codechef Snackdown Round 1A
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        long long n,k;
+        cin>>n>>k;
+        
+        long long z=n*(n-1);
+        z=z>>1;
+        long long temp=(n-k);
+        temp*=(temp-1);
+        temp>>=1;
+        long long ans=(z-temp)/k; 
+        ans*=2;
+        cout<<ans<<"\n";
+    }
+}
