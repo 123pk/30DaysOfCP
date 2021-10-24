d467fcf4a2b0e66cfc1e597292331d8561c75e37
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-24 20:21:04 +0530

    Update B. Luntik and Subsequences.cpp

--- Day27/B. Luntik and Subsequences.cpp
+++ Day27/B. Luntik and Subsequences.cpp
@@ -1,5 +1,6 @@
 /*
 Platform :- Codeforces
 Contest :-Codeforces Round 750 Div 2
-Approach :- 
+Approach :- Since we need to find those subsequences whose sum is (sum_of_array-1) so all this depend on count of '1' and count of '0' , let z = no_of_1 and x = no_of_zero
+            then answer is z*(2^x)  , take care of INT overflow
 */
