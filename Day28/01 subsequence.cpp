fa9b29ab31554da9c0daf2de19df4745bd1862ca
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-25 23:36:33 +0530

    Update 01 subsequence.cpp

--- Day28/01 subsequence.cpp
+++ Day28/01 subsequence.cpp
@@ -1,6 +1,6 @@
 /*
 Platform :- Codechef
-Contest :- Codechef Cook off October 2021
+Contest :- Codechef October Cook off  2021
 Approach :- Best strategy is to order elements at odd position in decreasing order and elements at even postion in increasing order , now for finding the total number of subsequence
             count total of even position element sum and run a loop from 0 to n-1 for all  odd poisitoin number of 01 subsequence position is (odd[i]*total_evel_elemts_from(i+1 to n))
 */
