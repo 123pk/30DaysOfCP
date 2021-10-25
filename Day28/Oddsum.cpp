f5e9de300cd1534f6921d47646aa35e98ed9e76b
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-25 22:58:00 +0530

    Create Oddsum.cpp

---
+++ Day28/Oddsum.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codechef
+Contest :- Octobers Cook off 2021
+Approach :- The array of that type is 1,0,2,4,6,...all even numbers so for n less than 3 we have 1 as sum while for all other values is (n-2)(n-1) + 1, sum_of_(n-2)even number+sum of firs 2
+*/
