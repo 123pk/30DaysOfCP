b9d246e1e5971fd3a1cfb73131568b31f8228d82
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-26 21:41:56 +0530

    Create invert binary tree.cpp

---
+++ Day29/invert binary tree.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Approach :- Store the pointer to root in some variable now perform bfs on the tree and swap left and right children and now return the varible that stored the pointer to root
+            take care of null root pointer.
+*/
