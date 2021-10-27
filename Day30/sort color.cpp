a51773dab5e7bf13aeb2ea29fa08db19d2df47ea
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-27 22:20:39 +0530

    Update sort color.cpp

--- Day30/sort color.cpp
+++ Day30/sort color.cpp
@@ -3,3 +3,23 @@ Platform :- Leetcode
 Approaach:- make a array of size 3 , now store the frequency of 0,1 and 2 in it now run a loop from ( 0 to nums.size()-1 ) and if frequency of 0 is not '0' store nums[i]=0 and decrease
             its frequency else move to '1' and do the same.
 */
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        vector<int>z(3);
+        for(auto &x:nums)z[x]++;
+        int j=0;
+        for(int i=0;i<nums.size();++i){
+            if(z[j])nums[i]=j;
+            else{
+                j++;
+                if(z[j])nums[i]=j;
+                else {
+                    j++;
+                    nums[i]=j;
+                }
+            }
+            z[j]--;
+        }
+    }
+};
