8d898411c78970685f2bc053542bb6cd7dac8551
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-10-27 22:20:24 +0530

    Create sort color.cpp

---
+++ Day30/sort color.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Approaach:- make a array of size 3 , now store the frequency of 0,1 and 2 in it now run a loop from ( 0 to nums.size()-1 ) and if frequency of 0 is not '0' store nums[i]=0 and decrease
+            its frequency else move to '1' and do the same.
+*/
